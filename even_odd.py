4e310839bb282a1d8d2270138459429cc22ca006
saiteja <saitej7288@gmail.com>
2025-08-20 13:18:52 +0530

    function

---
+++ even_odd.py
@@ -0,0 +1,15 @@
+def even_num(num):
+    if num%2==0:
+        print(f"{num} is a even number")
+    else:
+        print(f"{num} is not a even number")
+even_num(7)
+
+
+
+def odd_num(num):
+    if num%2!=0:
+        print(f"{num} is a odd number")
+    else:
+        print(f"{num} is not a odd number")
+odd_num(5)
