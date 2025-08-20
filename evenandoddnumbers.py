7a15760ba1ab7fffbc68ed1b1bef0163764c8015
saiteja <saitej7288@gmail.com>
2025-08-20 12:40:30 +0530

    even and odd program

---
+++ evenandoddnumbers.py
@@ -0,0 +1,15 @@
+num=int(input("enter the number"))
+for num in range(1,num+1):
+    if num%2==0:
+        print(f"{num} It is a even number")
+    else:
+        print(f"{num} It is a odd number ")
+
+
+
+# y=int(input.txt("enter the number"))
+# if y%2!=0:
+#     print(f"{y} it is a odd number")
+# else:
+#     print(f"{y} it is a not odd number")
+
