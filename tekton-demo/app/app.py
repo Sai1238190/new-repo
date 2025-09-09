e8f3ff09b88de16e3a9938a201ce2adadb2f69ed
saiteja <saitej7288@gmail.com>
2025-09-09 17:55:14 +0530

    files

--- tekton-demo/app/app.py
+++ tekton-demo/app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Tekton on OpenShift!", 200
+    return "Hello,I am from Tekton Python CI/CD"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
--- tekton-demo/app/test_app.py
+++ tekton-demo/app/test_app.py
@@ -1,7 +1,7 @@
 from app import app
 
-def test_index():
+def test_root():
     client = app.test_client()
-    resp = client.get('/')
-    assert resp.status_code == 200
-    assert b"Hello from Tekton" in resp.data
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello,I am from Tekton Python CI/CD" in response.data
